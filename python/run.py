7737fbeb8e97228381823f6c1297a3d3d3a4f72c
Alexey Kotlyarov <koterpillar@gmail.com>
2012-08-18 17:42:11 +1000

    Python - count the results.

--- python/run.py
+++ python/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from pprint import pprint
 from random import random
 import string
 import sys
@@ -21,4 +22,18 @@ def generate(depth):
             result[item()] = generate(depth - 1)
         return result
 
+def count(tree):
+    accum = {it: 0 for it in items}
+    def count_accum(tree):
+        if type(tree) is str:
+            accum[tree] += 1
+        else:
+            for k, v in tree.items():
+                accum[k] += 1
+                count_accum(v)
+    count_accum(tree)
+    return accum
+
 tree = generate(int(sys.argv[1]))
+counts = count(tree)
+pprint(counts)
